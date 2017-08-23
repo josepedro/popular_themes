dddd11ae80ccf3bc0053af7bccc7d81b39be47ae
José Pedro <1jpsneto@gmail.com>
2017-08-23 18:58:08 -0300

    Fix views

--- popular_themes/views.py
+++ popular_themes/views.py
@@ -14,20 +14,24 @@ def sort_themes_by_score():
     for theme in Theme.objects.all():
         scores_themes[theme.id] = 0
     year_now = timezone.now().year
-    date_time_reference = datetime(year=year_now,month=1,day=1,hour=0,minute=00)
+    date_time_reference = timezone.now()
     for video in videos_list:
-        date_since_upload = video.date_uploaded - date_time_reference 
+        date_since_upload = date_time_reference - video.date_uploaded
         days_since_upload = date_since_upload.days
         time_factor = max(0, 1 - (days_since_upload/365))
         views = video.views
         comments = Comment.objects.filter(video=video)
-        positive_comments = len([comment for comment in comments if comment.is_positive == True])
-        negative_comments = len([comment for comment in comments if comment.is_positive == False])
-        good_comments = positive_comments/(positive_comments+negative_comments)
+        good_comments = 0
+        if len(comments) != 0:
+            positive_comments = len([comment for comment in comments if comment.is_positive == True])
+            negative_comments = len([comment for comment in comments if comment.is_positive == False])
+            good_comments = positive_comments/(positive_comments+negative_comments)
         thumbs = Thumb.objects.filter(video=video)
-        thumbs_up = len([thumb for thumb in thumbs if thumb.is_positive == True])
-        thumbs_down = len([thumb for thumb in thumbs if thumb.is_positive == False])
-        thumbs_up = thumbs_up/(thumbs_up+thumbs_down)
+        thumbs_up = 0
+        if len(thumbs) != 0:
+            thumbs_up = len([thumb for thumb in thumbs if thumb.is_positive == True])
+            thumbs_down = len([thumb for thumb in thumbs if thumb.is_positive == False])
+            thumbs_up = thumbs_up/(thumbs_up+thumbs_down)
         positivity_factor = 0.7 * good_comments + 0.3 * thumbs_up
         score = views*time_factor*positivity_factor 
         themes = video.themes.all()
