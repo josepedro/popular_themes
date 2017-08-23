3b70e5611239d68f635a67464fb3065dddde670d
José Pedro <1jpsneto@gmail.com>
2017-08-23 13:13:45 -0300

    Adding others model

--- popular_themes/models.py
+++ popular_themes/models.py
@@ -1,4 +1,20 @@
 from django.db import models
 
 class Theme(models.Model):
-	name =  models.CharField(max_length=200)
+    name =  models.CharField(max_length=200)
+
+class Video(models.Model):
+    name =  models.CharField(max_length=200)
+    date_uploaded = models.DateTimeField('date uploaded', auto_now_add=True)
+    views = models.IntegerField(default=0)
+    themes = models.ManyToManyField(Theme)
+
+class Thumb(models.Model):
+    is_positive = models.BooleanField()
+    time = models.DateTimeField('time thumb', auto_now_add=True)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    is_positive = models.BooleanField()
+    time = models.DateTimeField('time comment', auto_now_add=True)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
--- popular_themes/tests.py
+++ popular_themes/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from datetime import datetime
 
 from .models import Theme
+from .models import Video
+from .models import Comment
+from .models import Thumb
 
 class ThemeIndexViewTests(TestCase):
     
@@ -19,6 +23,26 @@ class ThemeIndexViewTests(TestCase):
         response = self.client.get(reverse('popular_themes:get_popular_themes'))
         theme_list = response.context['sorted_theme_list']
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(theme_list[0].name, theme_1.name)
-        self.assertEqual(theme_list[1].name, theme_2.name)
         self.assertContains(response, theme_1.name)
+
+    def test_list_sorted_themes(self):
+        # Check if accounts appear
+        date = datetime.now
+        theme_1 = Theme.objects.create(name='test_1')
+        theme_2 = Theme.objects.create(name='test_2')
+        video_1 = Video.objects.create(name='video_1', views=5, date_uploaded=date)
+        video_1.themes.add(theme_2)
+        video_2 = Video.objects.create(name='video_2', views=10, date_uploaded=date)
+        video_2.themes.add(theme_1)
+        video_2.themes.add(theme_2)
+        thumb_1 = Thumb.objects.create(is_positive=False, video=video_1, time=date)
+        thumb_2 = Thumb.objects.create(is_positive=True, video=video_2, time=date)
+        comment_1 = Comment.objects.create(is_positive=True, video=video_1, time=date)
+        comment_2 = Comment.objects.create(is_positive=True, video=video_2, time=date)
+
+        response = self.client.get(reverse('popular_themes:get_popular_themes'))
+        theme_list = response.context['sorted_theme_list']
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(theme_list[0].name, theme_2.name)
+        self.assertEqual(theme_list[1].name, theme_1.name)
+        self.assertContains(response, theme_2.name)
